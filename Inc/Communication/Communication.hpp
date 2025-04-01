cf0127709f7223b84fc759aaa7d58550fd970431
Ikubimu <ikerubidemunoz@gmail.com>
2025-04-01 15:55:02 +0200

    update code with control1dof orders

--- Inc/Communication/Communication.hpp
+++ Inc/Communication/Communication.hpp
@@ -4,15 +4,24 @@
 
 #define SPI_RS_PIN PB2
 
-#define EN_LDU_ID 9998
 #define SET_LDU_ID 9999
+#define EN_LDU_ID 9998
+#define RST_LDU_ID 9997
 #define STATE_ID 9996
+#define DIS_LDU_ID 9996
+#define CURR_LDU_ID 9995
+#define START_CONTROL_ID 9994
+#define STOP_CONTROL_ID 9993
+#define START_PWM_ID 9992
+#define STOP_PWM_ID 9991
+
 #define SEND_STATE_RECV_LPU_ID 8888
 #define SET_DESIRED_CURRENT_ID 8889
 #define START_CONTROL_SLAVE_ID 8887
 #define STOP_CONTROL_SLAVE_ID 8886
 #define START_PWM_SLAVE_ID 8885
 #define STOP_PWM_SLAVE_ID 8884
+#define RECEIVE_AIRGAP_ID 8883
 
 extern uint8_t set_ldu_spi_id;
 extern uint8_t en_ldu_spi_id;
@@ -20,9 +29,12 @@ extern uint16_t state_id;
 
 #define PACKET_LDU_TYPE uint8_t, float, uint32_t
 #define SHUNT_ARR_TYPE float, float, float, float, float, float, float, float, float, float
+#define AIRGAP_ARR_TYPE float, float, float, float, float, float, float, float
 #define VBAT_ARR_TYPE float, float, float, float, float, float, float, float, float, float
 #define LDU_REF_ARR_TYPE float, float, float, float, float, float, float, float, float, float
 #define LDU_EXIT_ARR_TYPE float, float, float, float, float, float, float, float, float, float
+#define DIS_REF_ARR_TYPE float, float, float, float, float, float, float, float
+#define DIS_EXIT_ARR_TYPE float, float, float, float, float, float, float, float
 
 extern uint8_t curr_state;
 
@@ -33,6 +45,7 @@ class SPI_DATA
     static uint8_t id_ldu;
     static float duty;
     static float desired_current;
+    static float desired_distance;
     static uint32_t frequency;
     static int64_t datetime;
     static uint8_t id_buffer;
@@ -42,15 +55,18 @@ class SPI_DATA
 
     static uint8_t spi_id;
     static SPIBasePacket *LDU_packet;
+    static SPIBasePacket *id_ldu_packet;
     static SPIBasePacket *id_buffer_packet;
     static SPIBasePacket *state_packet;
     static SPIBasePacket *nonePacket;
     static SPIBasePacket *en_buffer_packet;
     static SPIBasePacket *data_LPU_slave_packet;
+    static SPIBasePacket *data_arigap_packet;
     static SPIBasePacket *current_ldu_packet;
 
     static SPIStackOrder* LDU_order;
     static SPIStackOrder* en_LDU_buffer_order;
+    static SPIStackOrder* receive_data_airgap_order;
     static SPIStackOrder* send_state_receive_data_lpu_order; 
     static SPIStackOrder* initial_order;
     static SPIStackOrder* set_current_order;
@@ -61,17 +77,17 @@ class SPI_DATA
 
     static unordered_map<uint8_t, SPIBasePacket*> packets;
     static float shunt_arr[10];
+    static float airgap_arr[8];
     static float vbat_arr[10];
     static float ldu_ref[10];
     static float ldu_exit[10];
+    static float dis_exit[8];
+    static float dis_ref[8];
 
 
 
     static void inscribe_spi();
 
-    static void send_initial_check();
-
     static void start();
-    static void send_ldu_pwm();
 
 };
--- Src/Communication/Communication.cpp
+++ Src/Communication/Communication.cpp
@@ -11,17 +11,21 @@ uint8_t SPI_DATA::id_buffer = 0;
 
 uint8_t SPI_DATA::spi_id = 0;
 uint8_t SPI_DATA::en_buffer_byte = 0;
+float SPI_DATA::desired_distance = 0.0;
 
 SPIBasePacket* SPI_DATA::LDU_packet = nullptr;
 SPIBasePacket* SPI_DATA::id_buffer_packet = nullptr;
+SPIBasePacket* SPI_DATA::id_ldu_packet = nullptr;
 SPIBasePacket* SPI_DATA::state_packet = nullptr;
 SPIBasePacket* SPI_DATA::data_LPU_slave_packet = nullptr;
+SPIBasePacket* SPI_DATA::data_arigap_packet = nullptr;
 SPIBasePacket* SPI_DATA::nonePacket = nullptr;
 SPIBasePacket* SPI_DATA::en_buffer_packet = nullptr;
 SPIBasePacket* SPI_DATA::current_ldu_packet = nullptr;
 
 SPIStackOrder* SPI_DATA::LDU_order = nullptr;
 SPIStackOrder* SPI_DATA::en_LDU_buffer_order = nullptr;
+SPIStackOrder* SPI_DATA::receive_data_airgap_order = nullptr;
 SPIStackOrder* SPI_DATA::initial_order = nullptr;
 SPIStackOrder* SPI_DATA::send_state_receive_data_lpu_order = nullptr;
 SPIStackOrder* SPI_DATA::set_current_order = nullptr;
@@ -40,6 +44,11 @@ float SPI_DATA::shunt_arr[10] = {
 float SPI_DATA::vbat_arr[10] = {};
 float SPI_DATA::ldu_ref[10] = {};
 float SPI_DATA::ldu_exit[10] = {};
+float SPI_DATA::dis_ref[8] = {};
+float SPI_DATA::dis_exit[8] = {};
+
+float SPI_DATA::airgap_arr[8] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+
 
 
 
@@ -56,42 +65,44 @@ void SPI_DATA::inscribe_spi()
 
 void SPI_DATA::start()
 {
+    SPI::start();
     LDU_packet = new SPIPacket<9, PACKET_LDU_TYPE>(&id_ldu, &duty, &frequency);
-
+    
+    id_ldu_packet = new SPIPacket<5, uint8_t, float>(&id_ldu, &desired_distance);
     id_buffer_packet = new SPIPacket<1, uint8_t>(&id_buffer);
     state_packet = new SPIPacket<1, uint8_t>(&curr_state);
-    data_LPU_slave_packet = new SPIPacket<160, SHUNT_ARR_TYPE, VBAT_ARR_TYPE, LDU_REF_ARR_TYPE, LDU_EXIT_ARR_TYPE>(
+    data_LPU_slave_packet = new SPIPacket<224, SHUNT_ARR_TYPE, VBAT_ARR_TYPE, LDU_REF_ARR_TYPE, LDU_EXIT_ARR_TYPE, DIS_REF_ARR_TYPE, DIS_EXIT_ARR_TYPE>(
         &shunt_arr[0], &shunt_arr[1], &shunt_arr[2], &shunt_arr[3], &shunt_arr[4], &shunt_arr[5], &shunt_arr[6], &shunt_arr[7], &shunt_arr[8], &shunt_arr[9],
         &vbat_arr[0], &vbat_arr[1], &vbat_arr[2], &vbat_arr[3], &vbat_arr[4], &vbat_arr[5], &vbat_arr[6],&vbat_arr[7], &vbat_arr[8], &vbat_arr[9],
         &ldu_ref[0], &ldu_ref[1], &ldu_ref[2], &ldu_ref[3], &ldu_ref[4], &ldu_ref[5], &ldu_ref[6], &ldu_ref[7], &ldu_ref[8], &ldu_ref[9],
-        &ldu_exit[0], &ldu_exit[1], &ldu_exit[2], &ldu_exit[3], &ldu_exit[4], &ldu_exit[5], &ldu_exit[6], &ldu_exit[7], &ldu_exit[8], &ldu_exit[9]
+        &ldu_exit[0], &ldu_exit[1], &ldu_exit[2], &ldu_exit[3], &ldu_exit[4], &ldu_exit[5], &ldu_exit[6], &ldu_exit[7], &ldu_exit[8], &ldu_exit[9],
+        &dis_ref[0], &dis_ref[1], &dis_ref[2], &dis_ref[3], &dis_ref[4], &dis_ref[5], &dis_ref[6], &dis_ref[7],
+        &dis_exit[0], &dis_exit[1], &dis_exit[2], &dis_exit[3], &dis_exit[4], &dis_exit[5], &dis_exit[6], &dis_exit[7]
     );
+    data_arigap_packet = new SPIPacket<32, AIRGAP_ARR_TYPE>(&airgap_arr[0], &airgap_arr[1], &airgap_arr[2], &airgap_arr[3], &airgap_arr[4], &airgap_arr[5], &airgap_arr[6], &airgap_arr[7]);
     en_buffer_packet = new SPIPacket<1, uint8_t>(&en_buffer_byte);
     nonePacket = new SPIPacket<0>;
+
     current_ldu_packet = new SPIPacket<5, uint8_t, float>(&id_ldu, &desired_current);
+
+
+    initial_order = new SPIStackOrder(STATE_ID, *state_packet, *nonePacket);
+
+
+    LDU_order = new SPIStackOrder(SET_LDU_ID, *LDU_packet, *nonePacket);
+    send_state_receive_data_lpu_order = new SPIStackOrder(SEND_STATE_RECV_LPU_ID, *state_packet, *data_LPU_slave_packet);
+    receive_data_airgap_order = new SPIStackOrder(RECEIVE_AIRGAP_ID, *nonePacket, *data_arigap_packet);
 
     en_LDU_buffer_order = new SPIStackOrder(EN_LDU_ID, *en_buffer_packet, *nonePacket); 
 
-    initial_order = new SPIStackOrder(STATE_ID, *state_packet, *nonePacket);
-    LDU_order = new SPIStackOrder(SET_LDU_ID, *LDU_packet, *nonePacket);
-    
-
-
-    send_state_receive_data_lpu_order = new SPIStackOrder(SEND_STATE_RECV_LPU_ID, *state_packet, *data_LPU_slave_packet);
-
-    
-
     set_current_order = new SPIStackOrder(SET_DESIRED_CURRENT_ID, *current_ldu_packet, *nonePacket);
-    
 
-    start_control_order = new SPIStackOrder(START_CONTROL_SLAVE_ID, *nonePacket, *nonePacket);
-    
+    start_control_order = new SPIStackOrder(START_CONTROL_SLAVE_ID, *id_ldu_packet, *nonePacket);
     stop_control_order = new SPIStackOrder(STOP_CONTROL_SLAVE_ID, *nonePacket, *nonePacket);
-    
+
     start_pwm_order = new SPIStackOrder(START_PWM_SLAVE_ID, *current_ldu_packet, *nonePacket);
-    
     stop_pwm_order = new SPIStackOrder(STOP_PWM_SLAVE_ID, *current_ldu_packet, *nonePacket);
-    
+
 
 
 }
